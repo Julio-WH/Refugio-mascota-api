65b08881282e0cb42db3460715a0cfff366e0c23
Julio-WH <julio@wisphub.net>
2022-05-17 14:33:47 -0500

    se cambio el nombre del proyecto

--- Refugio-mascota/urls.py
+++ Refugio-mascota/urls.py
@@ -1,4 +1,4 @@
-"""Test URL Configuration
+"""Refugio-mascota URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.9/topics/http/urls/
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url,include
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
